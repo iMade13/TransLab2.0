85918cdfecf61eb33d451856ed70d29c5caaad46
Madeleine Marcano <madeleine1302@gmail.com>
2018-08-05 09:50:09 -0400

    jquery para el menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -2,15 +2,13 @@ $(function() {
     let boton = $('#btn-menu');
     let fondo_enlace = $('#fondo-enlace');
 
-    boton.on('click', function(e) {
+    boton.on('click', function() {
         fondo_enlace.toggleClass('active');
         $('#menu-lateral').toggleClass('active');
-        e.preventDefault();
     });
 
-    fondo_enlace.on('click', function(e) {
+    fondo_enlace.on('click', function() {
         fondo_enlace.toggleClass('active');
         $('#menu-lateral').toggleClass('active');
-        e.preventDefault();
     });
 }())
