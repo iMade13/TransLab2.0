c6ac29044bf9f521e0233f655fe14c066e66a8ee
Madeleine Marcano <madeleine1302@gmail.com>
2018-08-06 12:51:12 -0400

    cambios varios

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 ## Aplicación móvil para consultar saldo de la tarjeta BIP!
 
 ### Introducción
-Para su uso es necesario contar con un usuario y contraseña con máximo 8 caracteres y solo números. Cuenta con un pérfil con el correo y foto, donde el usuario podrá agregar todas las tarjetas BIP que tenga. 
-En el menú podrás acceder a **Ver Saldo**, **Cálcular Tarifa**, **Perfil**, y saber cuales son las **Preguntas Frecuentes**. 
+Para su uso es necesario contar con un usuario y contraseña con máximo 8 caracteres y solo números. Cuenta con un perfil con el correo y foto, donde el usuario podrá agregar todas las tarjetas BIP que tenga. 
+En el menú podrá acceder a **Ver Saldo**, **Calcular Tarifa**, **Perfil**, y saber cuales son las **Preguntas Frecuentes**. 
 
 Tendrás la opción de ver el saldo disponible de tus tarjetas afiliadas, como también cálcular la tarifa que corresponde seleccionando el horario del viaje que desea realizar. 
 En preguntas frecuentes podrás consultar las dudas que tengas sobre como utilizar la aplicación. 
@@ -16,8 +16,8 @@ Para su acceso:
 
 ### Planificación
 
-Para la organización y planificación de las tareas que se llevaron a cabo se utilizó Trello como herramienta principal. 
-Pueden acceder a través del enlace del siguiente enlace: [Trello](https://trello.com/invite/b/w9r6EmM0/f1b7420b1b82f8ed3b917410ec23d72c/track-translab20)
+Para la planificación y organización de las tareas que se llevaron a cabo se utilizó Trello como herramienta principal. 
+Pueden acceder a través del siguiente enlace: [Trello](https://trello.com/invite/b/w9r6EmM0/f1b7420b1b82f8ed3b917410ec23d72c/track-translab20)
 
 *********************************************************************************************************************
 
--- src/js/login.js
+++ src/js/login.js
@@ -1,15 +1,72 @@
-window.validatePass = function(password) {
-    if (typeof(parseInt(password)) != 'number') {
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAFhc-grSOcnOicd-FYECe4KbBnZMEssoU",
+    authDomain: "translab-made.firebaseapp.com",
+    databaseURL: "https://translab-made.firebaseio.com",
+    projectId: "translab-made",
+    storageBucket: "translab-made.appspot.com",
+    messagingSenderId: "749339486306"
+};
+firebase.initializeApp(config);
+
+window.onload = () => {
+        firebase.auth().onAuthStateChanged(user => {
+            if (user) {
+                currentUser = firebase.auth().currentUser
+            }
+        });
+    }
+    //Login
+function loginWithFirebase() {
+    const emailValue = inputEmail.value
+    const passwordValue = inputPassword.value
+
+    firebase.auth().signInWithEmailAndPassword(emailValue, passwordValue)
+        .then((e) => {
+            console.log('usuario inicio sesion con exito')
+
+        })
+        .catch((error) => {
+            console.log('error de firebase > codigo ' + error.message)
+        })
+}
+
+function redirectFromLogin() {
+    location.href = "home.html";
+}
+
+/* --------- FUNCIONES DEL TEST  ------- */
+window.validatePass = function(passwordValue) {
+    const passwordValue = inputPassword.value
+    if (typeof(parseInt(passwordValue)) != 'number') {
         return false;
     }
-    if (password.legth > 8) {
+    if (passwordValue.legth > 8) {
         return false;
     }
     return true;
-
 }
 
 window.calculateRate = function(balance, rate) {
-
     return balance - rate;
 }
+
+/*--------- FIN DEL TEST -----*/
+
+
+// function registerWithFirebase() {
+//     firebase.auth().createUserWithEmailAndPassword(emailValue, passwordValue)
+//         // let passwordValue = document.getElementById('inputPassword').value.length
+//     const emailValue = inputEmail.value
+//     const passwordValue = inputPassword.value
+//         // if (passwordValue > 8 || typeof(parseInt(passwordValue)) != 'number') {
+//         //     alert('Su password debe tener máximo 8 números')
+//         .then(() => {
+//             console.log('usuario creado con exito')
+//             redirectFromLogin()
+//         })
+//         .catch((error) => {
+//             console.log('error de firebase > codigo ' + error.message)
+//         })
+//     console.log(passwordValue);
+// }
