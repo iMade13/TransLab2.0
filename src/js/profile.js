8b05bced247bd485df5eaf5f5fc809099bc0b4ff
Madeleine Marcano <madeleine1302@gmail.com>
2018-08-06 18:35:09 -0400

    agregando select a perfil

--- src/js/profile.js
+++ src/js/profile.js
@@ -1,30 +1,30 @@
-firebase.auth().onAuthStateChanged((user) => {
-    if (user) {
-        userMail.innerText = user.email;
-        // userPhoto.innerText = user.photoURL;
-    } else {
-        console.log('User > ' + JSON.stringify(user));
-    }
-    saveCard = () => {
-        let numberCard = document.getElementById("cardNumber").value;
-        firebase.database().ref(`users/${user.uid}`).child('Bip').push(numberCard);
-    }
-});
+var config = {
+    apiKey: "AIzaSyAFhc-grSOcnOicd-FYECe4KbBnZMEssoU",
+    authDomain: "translab-made.firebaseapp.com",
+    databaseURL: "https://translab-made.firebaseio.com",
+    projectId: "translab-made",
+    storageBucket: "translab-made.appspot.com",
+    messagingSenderId: "749339486306"
+};
+firebase.initializeApp(config);
 
+window.onload = () => {
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            userMail.innerText = user.email;
+            // userPhoto.innerText = user.photoURL;
+        } else {
+            console.log('User > ' + JSON.stringify(user));
+        }
+        saveCard = () => {
+            let numberCard = document.getElementById("cardNumber").value;
+            document.getElementById('cardNumber').value = '';
+            firebase.database().ref(`users/${user.uid}`).child('NúmeroBip').push(numberCard);
 
-const button = document.getElementById('btn-add');
-
-button.addEventListener('click', () => {
-    let cardBip = document.getElementById('cardNumber').value;
-    document.getElementById('cardNumber').value = '';
-
-    const contCard = document.getElementById('contCard');
-    // const contNew = document.createElement('div');
-    const contNew = document.createElement('select');
-    const newCard = document.createElement('option');
-    let numberCard = document.createTextNode(cardBip);
-    newCard.appendChild(numberCard);
-    // contNew.appendChild(newCard);
-    // contCard.appendChild(contNew);
-
-})
+            const contCard = document.getElementById('contCard');
+            const option = document.createElement('option');
+            option.text = numberCard;
+            contCard.add(option, contCard[0]);
+        }
+    });
+}
