96e9a6ee98a2b50cc424f51b9aa9d4b1f7451573
Madeleine Marcano <madeleine1302@gmail.com>
2018-08-06 14:50:06 -0400

    cambios en index funcion login pruebas2

--- src/js/login.js
+++ src/js/login.js
@@ -1,13 +1,13 @@
 // Initialize Firebase
-var config = {
-    apiKey: "AIzaSyAFhc-grSOcnOicd-FYECe4KbBnZMEssoU",
-    authDomain: "translab-made.firebaseapp.com",
-    databaseURL: "https://translab-made.firebaseio.com",
-    projectId: "translab-made",
-    storageBucket: "translab-made.appspot.com",
-    messagingSenderId: "749339486306"
-};
-firebase.initializeApp(config);
+// var config = {
+//     apiKey: "AIzaSyAFhc-grSOcnOicd-FYECe4KbBnZMEssoU",
+//     authDomain: "translab-made.firebaseapp.com",
+//     databaseURL: "https://translab-made.firebaseio.com",
+//     projectId: "translab-made",
+//     storageBucket: "translab-made.appspot.com",
+//     messagingSenderId: "749339486306"
+// };
+// firebase.initializeApp(config);
 
 window.onload = () => {
     firebase.auth().onAuthStateChanged(user => {
