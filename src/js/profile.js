bf1f4c7a2732d3386f8898c5e150da7229a38b75
Madeleine Marcano <madeleine1302@gmail.com>
2018-08-06 19:07:37 -0400

    cambios de archivo de la api

--- src/js/profile.js
+++ src/js/profile.js
@@ -28,3 +28,19 @@ window.onload = () => {
         }
     });
 }
+cargarAPI = () => {
+    const numberCardBip = document.getElementById('cardNumber').value;
+    document.getElementById('cardNumber').value = '';
+
+
+    fetch(`http://www.psep.cl/api/Bip.php?&numberBip=${numberCardBip}`)
+        .then(function(res) {
+            return res.json();
+        })
+        .then(function(data) {
+            const dataBip = Object.values(data)
+
+            let amountBip = dataBip[2];
+            document.getElementById("verSaldo").innerHTML = `Saldo total  </br> ${amountBip}`;
+        })
+}
