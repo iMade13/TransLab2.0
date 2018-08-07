35b5e40b1b3d813e722336d74cb465cc475ee4b3
Madeleine Marcano <madeleine1302@gmail.com>
2018-08-06 23:51:59 -0400

    cambios en calcular.html

--- src/js/main.js
+++ src/js/main.js
@@ -38,24 +38,29 @@ window.onload = () => {
 
 
 cargarAPI = () => {
-    const numberCardBip = document.getElementById('cardNumber').value;
-    document.getElementById('cardNumber').value = '';
+        const numberCardBip = document.getElementById('cardNumber').value;
+        document.getElementById('cardNumber').value = '';
 
 
-    fetch(`http://www.psep.cl/api/Bip.php?&numberBip=${numberCardBip}`)
-        .then(function(res) {
-            return res.json();
-        })
-        .then(function(data) {
-            const dataBip = Object.values(data)
+        fetch(`http://www.psep.cl/api/Bip.php?&numberBip=${numberCardBip}`)
+            .then(function(res) {
+                return res.json();
+            })
+            .then(function(data) {
+                const dataBip = Object.values(data)
 
-            let amountBip = dataBip[2];
-            document.getElementById("verSaldo").innerHTML =
-                `<span class='col d-flex justify-content-center' id='txtCosto'>COSTO PASAJE</span>
+                let amountBip = dataBip[2];
+                document.getElementById("verSaldo").innerHTML =
+                    `<span class='col d-flex justify-content-center' id='txtCosto'>COSTO PASAJE</span>
                 <span class='col d-flex justify-content-center' id='costoPasaje'>${amountBip}</span>`;
-        })
-        .then(function calcular(data) {
+            })
+            .then(function calcularSaldo() {
+                let datos = document.getElementById('contCard').value;
+                console.log(datos)
 
-
-        })
-}
+            })
+    }
+    // const calcularSaldo = () => {
+    //     let datos = document.getElementById('selectHorarios').value;
+    //     console.log(datos);
+    // }
