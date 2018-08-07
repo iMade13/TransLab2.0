ea520275e58a63d5dd4eb64d49da07d0f6cf2070
Madeleine Marcano <madeleine1302@gmail.com>
2018-08-06 23:28:50 -0400

    conflictos

--- src/js/main.js
+++ src/js/main.js
@@ -12,7 +12,7 @@ window.onload = () => {
     firebase.auth().onAuthStateChanged((user) => {
         if (user) {
             document.getElementById('userMail').innerText = user.email;
-            userPhotoo.innerText = URL("http://d28hp0i0mf9k3x.cloudfront.net/assets/default_person-3886b66ad5ca85d57ed2a0d12fd2b2e4.png")
+            userPhotoo.innerText = "http://d28hp0i0mf9k3x.cloudfront.net/assets/default_person-3886b66ad5ca85d57ed2a0d12fd2b2e4.png"
         } else {
             console.log('User > ' + JSON.stringify(user));
         }
